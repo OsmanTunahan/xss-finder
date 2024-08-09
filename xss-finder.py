f8712a756447f2a6f3d70c35221bd97dd84ba9dc
OsmanTunahan <osmantunahan007@gmail.com>
2024-08-09 13:06:57 +0300

    Add XSS attack and finder class

--- xss-finder.py
+++ xss-finder.py
@@ -243,3 +243,115 @@ class ConsoleOutput:
         for key in whole_dict:
             print(f"[+] Parameter: {key}, Suggested Payload: {whole_dict[key][0]['payload']}")
 
+
+class XssAttack:
+    def __init__(self, url, reflections, request_data, form_data, headers):
+        self.url = url
+        self.reflections = reflections
+        self.request_data = request_data
+        self.form_data = form_data
+        self.headers = headers
+
+    def execute(self, payloads):
+        if self.request_data:
+            self._execute_attack(payloads, self.request_data)
+        if self.form_data:
+            self._execute_attack(payloads, self.form_data)
+
+    def _execute_attack(self, payloads, data):
+        for key in data:
+            if key in payloads:
+                data[key] = payloads[key][0]['payload']
+                response = HttpRequest(custom_headers=self.headers).send(self.url, request_data=self.request_data, form_data=self.form_data)
+                print(f"[+] Attacking with payload {payloads[key][0]['payload']}")
+
+
+class XssFinder:
+    def __init__(self, args):
+        self.args = args
+        self.http_request = HttpRequest(args.user_agent, args.http_headers, args.http_cookie)
+        self.html_parser = HtmlParser()
+        self.payload_generator = PayloadGenerator()
+        self.classifier, self.vectorizer = ModelTrainer("./models/html_responses.csv").train()
+        self.scanner = XssScanner(self.classifier, self.vectorizer)
+
+    def run(self):
+        Banner.display()
+        signal.signal(signal.SIGINT, self.signal_handler)
+
+        ConsoleOutput.print_separator()
+        print("[+] XSS-Finder has started to crawl...")
+
+        for page in Crawler.crawl(self.args.target_url):
+            self.process_page(page)
+
+        ConsoleOutput.print_separator()
+        print("[+] Crawl completed.")
+
+    def process_page(self, page_url):
+        print(f"[+] Scanning URL: {page_url}")
+
+        html_content = self.http_request.send(page_url)
+        if not html_content:
+            print("[!] Failed to retrieve content.")
+            return
+
+        soup = self.html_parser.parse_html(html_content)
+        form_method, form_data = self.html_parser.parse_form(soup)
+
+        reflection_contexts = []
+        if self.args.reflection_param and self.args.reflection_value:
+            reflection_contexts.append((self.args.reflection_param, "Context"))
+
+        predicted_context = "Context"
+        if not reflection_contexts:
+            predicted_context = self.get_predicted_context(html_content)
+
+        payloads = self.payload_generator.generate(predicted_context)
+
+        if self.args.stealth_mode:
+            whole_dict = {}
+            for reflection_param, reflection_value in reflection_contexts:
+                whole_dict[reflection_param] = payloads
+
+            ConsoleOutput.print_payload_suggestions(whole_dict)
+        else:
+            attack = XssAttack(
+                page_url, 
+                reflection_contexts, 
+                self.args.http_data,
+                form_data, 
+                self.http_request.headers
+            )
+            attack.execute(payloads)
+
+    def get_predicted_context(self, html_content):
+        html_response_lines = html_content.splitlines()
+        contexts = [self.scanner.predict_context(line) for line in html_response_lines]
+        return self.scanner.get_majority_context(contexts)
+
+    def signal_handler(self, signum, frame):
+        print("[-] Ctrl+C detected. Stopping...")
+        sys.exit(0)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='XSS-Finder by @OsmanTunahan')
+    parser.add_argument('-t', '--target-url', required=True, help='Target URL')
+    parser.add_argument('-a', '--user-agent', help='User-Agent')
+    parser.add_argument('-d', '--http-data', help='HTTP Data')
+    parser.add_argument('-H', '--http-headers', help='HTTP Headers')
+    parser.add_argument('-C', '--http-cookie', help='HTTP Cookies')
+    parser.add_argument('-r', '--reflection-param', help='Reflection Parameter')
+    parser.add_argument('-v', '--reflection-value', help='Reflection Value')
+    parser.add_argument('-s', '--stealth-mode', action='store_true', help='Enable stealth mode')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    XssFinder(args).run()
+
+
+if __name__ == "__main__":
+    main()
